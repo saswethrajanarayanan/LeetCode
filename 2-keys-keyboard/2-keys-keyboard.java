82b62f96f1beb94f22213e9deb65a202017eeb7e
Sasweth Rajanarayanan <30744267+saswethrajanarayanan@users.noreply.github.com>
2021-02-04 12:10:50 +0530

    Time: 77 ms (5.51%), Space: 38.2 MB (15.19%) - LeetHub

---
+++ 2-keys-keyboard/2-keys-keyboard.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minSteps(int n) {
+        int[] dp = new int[n + 1];
+        for(int i = 2; i <= n; ++i) {
+            dp[i] = i;
+            for(int j = 2; j < i; ++j) {
+                if(i % j == 0) {
+                    dp[i] = Math.min(dp[i], dp[j] + i / j);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
