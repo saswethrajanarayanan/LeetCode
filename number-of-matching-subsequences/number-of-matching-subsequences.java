f09cf8b8c5b4d768777c6fdcbc706e7e9dcb7c08
Sasweth Rajanarayanan <30744267+saswethrajanarayanan@users.noreply.github.com>
2021-03-11 11:28:33 +0530

    Time: 302 ms (19.38%), Space: 39.6 MB (94.34%) - LeetHub

---
+++ number-of-matching-subsequences/number-of-matching-subsequences.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        if(s.equals("")) return 0;
+        if(words.length == 0) return 0;
+        int count = 0;
+        HashMap<String, Boolean> map = new HashMap();
+
+        for(String word : words) {
+            int index = 0;
+            if(map.containsKey(word)) {
+                if(map.get(word)) {
+                    count++;
+                    continue;
+                }
+            }
+            if(!map.containsKey(word)) {
+                for(int i = 0; i < s.length(); ++i) {
+                    if(word.charAt(index) == s.charAt(i)) {
+                        index++;
+                    }
+                    if(index >= word.length()) {
+                        map.put(word, true);
+                        count++;
+                        break;
+                    }
+                    map.put(word, false);
+                }
+            }
+        }
+        return count;
+    }
+}
