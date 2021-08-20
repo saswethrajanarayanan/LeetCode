af15374425bf4df1b0e2a188c0f4380687f9fa6c
Sasweth Rajanarayanan <30744267+saswethrajanarayanan@users.noreply.github.com>
2021-08-19 22:47:49 -0400

    Time: 0 ms (100.00%), Space: 37.1 MB (22.50%) - LeetHub

---
+++ sum-of-left-leaves/sum-of-left-leaves.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int value = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        dfs(root);
+        return value;
+    }
+    public void dfs(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        if(root.left != null && (root.left.left == null && root.left.right == null)) {
+            value += root.left.val;
+        }
+        dfs(root.left);
+        dfs(root.right);
+        return;
+    }
+}
