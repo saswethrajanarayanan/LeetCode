c94a0da3bc107b73a73eee951975477dd78aadb5
Sasweth Rajanarayanan <30744267+saswethrajanarayanan@users.noreply.github.com>
2021-09-13 21:56:20 -0400

    Time: 13 ms (5.86%), Space: 43.7 MB (23.65%) - LeetHub

---
+++ bulb-switcher-iv/bulb-switcher-iv.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minFlips(String target) {
+        char prev = '0';
+        int count = 0;
+        for(int i = 0; i < target.length(); ++i) {
+            if(target.charAt(i) != prev) {
+                count++;
+                prev = target.charAt(i);
+            }
+        }
+        return count;
+    }
+}
