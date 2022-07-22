20bbd00285e40cb4c2b6092f590fd927fff6c8e0
Sasweth Rajanarayanan <30744267+saswethrajanarayanan@users.noreply.github.com>
2022-07-22 17:52:23 -0500

    Time: 2 ms (65.12%), Space: 64.3 MB (83.75%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.java
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean removeOnes(int[][] grid) {
         for(int i = 0; i < grid.length; ++i) {
-            int[] row = grid[i];
-            int zerothElement = Math.abs(row[0] - grid[0][0]);
-            for(int j = 0; j < row.length; ++j) {
-                int nonZeroElement = Math.abs(row[j] - grid[0][j]);
+            int zerothElement = Math.abs(grid[i][0] - grid[0][0]);
+            for(int j = 0; j < grid[i].length; ++j) {
+                int nonZeroElement = Math.abs(grid[i][j] - grid[0][j]);
                 if(zerothElement != nonZeroElement) {
                     return false;
                 }
